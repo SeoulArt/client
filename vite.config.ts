01c2c674d678049090789de5cd33dc3324f6e443
kimyoungyin <yin199859@gmail.com>
2024-04-29 15:55:39 +0900

    issue #1 feat: remove vite.svg and add logo.svg

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
             components: "/src/components",
             assets: "/src/assets",
             hooks: "/src/hooks",
+            UI: "/src/UI",
         },
     },
 });
