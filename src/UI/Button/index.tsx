8850c86aff2fcb9515c50bac2d9cd95b7ba3f88d
kimyoungyin <yin199859@gmail.com>
2024-04-30 16:56:33 +0900

    issue #4 feat: create mypage layout without Oauth

--- src/UI/Button/index.tsx
+++ src/UI/Button/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import styles from "./index.module.css";
+import kakaoIcon from "assets/kakao.svg";
+import naverIcon from "assets/naver.svg";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
-    // type: "default" | "kakao" | "naver";
+    buttonType: "default" | "kakao" | "naver";
 }
 
-const Button = ({ children, type, ...rest }: ButtonProps) => {
+const Button = ({ children, buttonType = "default", ...rest }: ButtonProps) => {
+    const leftIcon = buttonType === "kakao" ? kakaoIcon : naverIcon;
+
     return (
-        <button
-            type={type}
-            className={`${styles.flex} ${styles.default}`}
-            {...rest}
-        >
+        <button className={`${styles.flex} ${styles[buttonType]}`} {...rest}>
+            {buttonType !== "default" && <img src={leftIcon} />}
             {children}
         </button>
     );
--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import Button from "UI/Button";
+import authStore from "store/authStore";
+import styles from "./index.module.css";
 
 const MyPage = () => {
-    return <div style={{ background: "red", height: 1000 }}> MyPage</div>;
+    const { user } = authStore();
+
+    const isAuthenticated = user !== null;
+
+    return (
+        <div className={styles.layout}>
+            <h2 className={styles.heading3}>프로필</h2>
+            <div className={styles.explains}>
+                <span>
+                    {isAuthenticated
+                        ? "로그인한 상태입니다."
+                        : "로그인한 상태가 아닙니다."}
+                </span>
+                <span>
+                    {isAuthenticated
+                        ? "로그아웃을 하고 싶으시면 아래 버튼을 눌러주세요."
+                        : "아래 간편 로그인을 통해 회원이 되어주세요."}
+                </span>
+            </div>
+            <div className={styles.buttons}>
+                {isAuthenticated ? (
+                    <Button buttonType="default">로그아웃</Button>
+                ) : (
+                    <>
+                        <Button buttonType="kakao">카카오로 계속하기</Button>
+                        <Button buttonType="naver">네이버로 계속하기</Button>
+                    </>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default MyPage;
