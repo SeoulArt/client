6f71253459781856e9ffbcea2e8b652082c84e08
kimyoungyin <yin199859@gmail.com>
2024-04-30 16:55:40 +0900

    issue #4 feat: set zustand for state management

---
+++ src/store/authStore.ts
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+
+interface User {
+    userId: number;
+    username: string;
+    role: "ROLE_ADMIN" | "ROLE_USER" | "ROLE_CREATOR";
+    profileImage: string;
+    description: string;
+}
+
+interface AuthStore {
+    user: null | User;
+    isLoading: boolean;
+}
+
+interface AuthAction {
+    login: (user: User) => void;
+}
+
+const authStore = create<AuthStore & AuthAction>((set) => ({
+    user: null,
+    isLoading: true,
+    login: (user) => {
+        set((state) => ({ ...state, user, isLoading: false }));
+    },
+    logout: () => {
+        set((state) => ({ ...state, user: null, isLoading: false }));
+    },
+}));
+
+export default authStore;
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
+            store: "/src/store",
             layout: "/src/layout",
             pages: "/src/pages",
             components: "/src/components",
