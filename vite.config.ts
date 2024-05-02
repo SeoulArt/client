c0b7312a396a4ea68adc731bd84c58235136dee4
kimyoungyin <yin199859@gmail.com>
2024-05-02 19:35:09 +0900

    issue #4 fix: fix CORS error about vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+console.log(process.env.VITE_API_URL);
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            store: "/src/store",
-            layout: "/src/layout",
-            pages: "/src/pages",
-            components: "/src/components",
-            assets: "/src/assets",
-            hooks: "/src/hooks",
-            UI: "/src/UI",
-            queries: "/src/queries",
+export default ({ mode }: { mode: string }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return defineConfig({
+        plugins: [react()],
+        server: {
+            proxy: {
+                "/back": {
+                    target: env.VITE_API_URL,
+                    changeOrigin: true,
+                    rewrite: (_path) => _path.replace(/^\/back/, ""),
+                },
+            },
         },
-    },
-});
+        resolve: {
+            alias: {
+                store: "/src/store",
+                layout: "/src/layout",
+                pages: "/src/pages",
+                components: "/src/components",
+                assets: "/src/assets",
+                hooks: "/src/hooks",
+                UI: "/src/UI",
+                queries: "/src/queries",
+            },
+        },
+    });
+};
