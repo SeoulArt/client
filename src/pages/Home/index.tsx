a5a07190af84a1fd40fb122edf97f6b87746afa0
kimyoungyin <yin199859@gmail.com>
2024-05-03 15:45:43 +0900

    issue #4 feat: slider reuseable component with react-slick

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,16 @@
+import ImgSlider from "components/ImgSlider";
+
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div>
+            <ImgSlider
+                images={[
+                    { src: "logo.svg", description: "로고 1" },
+                    { src: "logo.svg", description: "로고 2" },
+                ]}
+            />
+        </div>
+    );
 };
 
 export default Home;
