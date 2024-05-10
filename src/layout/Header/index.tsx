0426709281fcbe4af8f50eb836ce82c9de283c8f
kimyoungyin <yin199859@gmail.com>
2024-05-10 13:49:58 +0900

    issue #4 feat: user profileImage in header

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,8 +1,11 @@
 import Avatar from "assets/avatar.svg";
 import { Link } from "react-router-dom";
 import styles from "./index.module.css";
+import authStore from "store/authStore";
 
 const Header = ({ isOnMyPage }: { isOnMyPage: boolean }) => {
+    const { user } = authStore();
+
     return (
         <header className={styles.layout}>
             <div className={styles.flex}>
@@ -11,7 +14,14 @@ const Header = ({ isOnMyPage }: { isOnMyPage: boolean }) => {
                 </Link>
                 {isOnMyPage || (
                     <Link to={"/mypage"}>
-                        <img src={Avatar} alt="기본 프로필 이미지" />
+                        <img
+                            src={user?.profileImage || Avatar}
+                            alt={
+                                user
+                                    ? user.username + "님의 프로필 이미지"
+                                    : "기본 프로필 이미지"
+                            }
+                        />
                     </Link>
                 )}
             </div>
