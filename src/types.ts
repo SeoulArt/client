ff4099e4813f92d0fc26c08f369d2f028bbe0685
kimyoungyin <yin199859@gmail.com>
2024-05-26 16:00:17 +0900

    feat: add playList in User obj

--- src/types.ts
+++ src/types.ts
@@ -16,6 +16,7 @@ export interface User {
     profileImage: string;
     phoneNumber: string | null;
     ticketPlayList: { playId: PlayId | 2 | 4 | 6; ticketId: number }[];
+    playList: number[] | null;
 }
 
 export interface Token {
